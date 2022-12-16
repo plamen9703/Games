478001e046d322ad7235b9fa847267c306aca06b
plamen.hristov <plamen.hristov@LAB26-T.school.npmg.org>
2022-12-16 14:14:51 +0200

    push

--- Crossiroads/src/games/gui/crosyroads/Games.java
+++ Crossiroads/src/games/gui/crosyroads/Games.java
@@ -3,8 +3,7 @@ package games.gui.crosyroads;
 public class Games {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello there");
 	}
 
 }
